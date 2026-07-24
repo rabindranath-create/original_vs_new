dc80accc1d7ca35e97b28c1c98721b09d3c62b66
rabindranath-create <87770453+rabindranath-create@users.noreply.github.com>
2026-07-23 21:40:03 -0500

    Add files via upload

--- main_script15.R
+++ main_script15.R
@@ -12,31 +12,32 @@ rho <- 0.5
 dt <- 15
 
 keep_trav_cost <-c()
-keep_kendall <- c()
-keep_agree <- c()
+
+keep_rank <- list()
+keep_seq <- list()
+
 keep_margin <-c()
-keep_margin_dt <- c()
+
 
 
 for(ii in 1:100){
-
-  obs_gen_para <- read.csv(paste0("pattern/rho_", rho, "_", ii, ".csv"))
-  the_result <- DT_vs_fDT_Alg(obs_gen_para, dt)
   
-  keep_trav_cost <- c(keep_trav_cost, the_result$trav_cost_dif)
-  keep_kendall <- c(keep_kendall, the_result$kendall) 
-  keep_agree <- c(keep_agree, the_result$agreement)
-  keep_margin <- c(keep_margin, the_result$margin)
-  keep_margin_dt <- c(keep_margin_dt, the_result$margin_dt)
-
+  obs_gen_para <- read.csv(paste0("pattern/rho_", rho, "_", ii, ".csv"))
+  the_result <- fDT_Alg_Save(obs_gen_para, dt)
+  
+  keep_trav_cost <- c(keep_trav_cost, the_result$trav_cost)
+  
+  keep_rank[[as.character(ii)]] <- the_result$rank_edges
+  keep_seq[[as.character(ii)]] <- the_result$full_edge
+  
+  keep_margin <- c(keep_margin, the_result$avg_margin)
+  
 }
 
-final_result <- list(trav_cost = mean(keep_trav_cost),
-                     kendall = mean(keep_kendall), 
-                     agree = mean(keep_agree),
-                     margin = mean(keep_margin),
-                     margin_dt = mean(keep_margin_dt)
-                     )
+final_result <- list(trav_cost = keep_trav_cost,
+                     rank = keep_rank, 
+                     seq = keep_seq,
+                     margin = keep_margin)
 
 file_name <- file.path(output_dir, paste0("rho_", dt, "_", rho, ".rds"))
 saveRDS(final_result, file = file_name)
